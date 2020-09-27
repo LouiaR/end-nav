03bc2e549c97102116bfaed1917666426f7caa44
louiaR <louitiani@gmail.com>
2020-09-27 13:11:06 +0100

    Setup react client and express for ssr

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+const path = require("path");
+
+module.exports = {
+  entry: "./server/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "server.js",
+    publicPath: "/",
+  },
+  target: "node",
+  externals: nodeExternals(),
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: `'production'`,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
